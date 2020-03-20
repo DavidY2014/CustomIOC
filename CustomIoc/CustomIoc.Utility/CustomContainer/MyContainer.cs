14664b49af086e0f2cbae48d61db910d8f028ba7
david <wu.xiguanghua@163.com>
2020-03-21 00:47:33 +0800

    update container

--- CustomIoc/CustomIoc.Utility/CustomContainer/MyContainer.cs
+++ CustomIoc/CustomIoc.Utility/CustomContainer/MyContainer.cs
@@ -21,8 +21,24 @@ namespace CustomIoc.Utility.CustomContainer
         {
             string key = typeof(TFrom).FullName;
             Type type = this.myContainerDictionary[key];
+
+            #region 准备构造函数参数 ，实现对象之间关联
+
+            var ctor = type.GetConstructors()[0];
+            List<object> paraList = new List<object>();
+            foreach (var para in ctor.GetParameters())
+            {
+                Type paraType = para.ParameterType; // 获取参数的类型，比如IUserDAL
+                //创建这个类型
+                string paraKey = paraType.FullName;//IUserDAL的完整名称
+                Type paraTargetType = this.myContainerDictionary[paraKey];
+                paraList.Add(Activator.CreateInstance(paraTargetType));
+            }
+            #endregion
+            object oInstance = Activator.CreateInstance(type, paraList.ToArray());
+
             //通过类型，类来生成对象，反射
-            object oInstance = Activator.CreateInstance(type);
+            //object oInstance = Activator.CreateInstance(type);
             return (TFrom)oInstance;
         }
     }
